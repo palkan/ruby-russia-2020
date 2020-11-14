437e9df98cc2eeeb1456ed07dd135ba47e6be411
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-11-14 20:42:54 +0300

    Configure Action Cable

---
+++ app/frontend/init/cable.js
@@ -0,0 +1,16 @@
+import { createConsumer } from '@rails/actioncable'
+
+let consumer
+
+export const createCable = () => {
+  if (!consumer) {
+    consumer = createConsumer()
+  }
+
+  return consumer
+}
+
+export const createChannel = (...args) => {
+  const consumer = createCable()
+  return consumer.subscriptions.create(...args)
+}
