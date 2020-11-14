bfa3884442b8b3be3eb86223e74124ff5f05ac13
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-11-14 22:27:05 +0300

    Migrate aside to reflex

--- app/reflexes/application_reflex.rb
+++ app/reflexes/application_reflex.rb
@@ -8,6 +8,8 @@ class ApplicationReflex < StimulusReflex::Reflex
 
   around_reflex :set_current_user, :set_current_track
 
+  private
+
   def set_current_user
     with_current_user(connection.user) { yield }
   end
@@ -15,5 +17,11 @@ class ApplicationReflex < StimulusReflex::Reflex
   def set_current_track
     track = Track.find_by(id: session[:track_id])
     with_current_track(track) { yield }
+  end
+
+  def component(name, **kwargs)
+    component_class = "#{name.camelize}::Component".constantize
+
+    ApplicationController.render(component_class.new(**kwargs), layout: false)
   end
 end
